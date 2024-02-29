739c2a29ceb0bf770b7289152dceafb34a61daea
Matthias Berg-Neels <matthiasuweberg@icloud.com>
2024-02-29 17:30:01 +0100

    Enhancement description in NotEnoughFreeSlotsException as comments

--- src/lecture/chapter8/NotEnoughFreeSlotsException.java
+++ src/lecture/chapter8/NotEnoughFreeSlotsException.java
@@ -2,6 +2,13 @@ package lecture.chapter8;
 
 public class NotEnoughFreeSlotsException extends Exception {
 
+  /*
+  Aufgaben zur Erweiterung:
+  1. NotEnoughFreeSlotsException in Airplane.book() einbauen und auslösen
+  2. Meldung aus NotEnoughFreeSlotsException um die Anzahl der freiePlätze und versuchten gebuchten Plätze erweitern
+  3. catch-Block in Travel Agency: verbleibende freie Plätze buchen
+   */
+  
   public NotEnoughFreeSlotsException(){
     super("Nicht genug freie Plätze!");
   }
